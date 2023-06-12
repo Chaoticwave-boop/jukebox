5508fa5fac5368353d08dc93c48db37d062b54e3
Chaoticwave-boop <bren.stoute@hotmail.com>
2023-06-12 11:26:55 +0200

    start music page en background added

--- jukeboxmpa/src/jukeboxhome/main.js
+++ jukeboxmpa/src/jukeboxhome/main.js
@@ -4,8 +4,12 @@ import './main.scss';
 
 const Home = () => {
     return (
-        <div>
-            <h1>this is the home page</h1>
+        <div className="home_page">
+            <div className="mainpage">
+                <h1>this is the home page</h1>
+                <p>this is a website where you can save music</p>
+                <p>go to the music tab to add your favorite songs!</p>
+            </div>
         </div>
     )
 }
--- jukeboxmpa/src/jukeboxmusic/music.js
+++ jukeboxmpa/src/jukeboxmusic/music.js
@@ -1,15 +1,69 @@
 import React from "react";
+import './music.scss';
+import { useState } from "react";
 
+import {TextField, Button, Box} from '@mui/material';
 
 
 const Music = () => {
+    const [music, setMusic] = useState([
+        {
+            name: "thunderstruck",
+            artist:"acdc" ,
+            genre: "rock",
+            file: "acdc.mp3",
+    
+        },
+        {
+            name: "i dont wanna hear it",
+            artist:"minor threat" ,
+            genre: "rock",
+            file: "minor.mp3",
+        }
+    ]);
+
     return (
-        <div>
-            <h1>here is music</h1>
+        <div className="music_page">
+            <div className="musictext">
+                <h1>Add your music and search</h1>
+                <SearchBar/>
+
+                <MusicBox music={music} />
+            </div>
 
         </div>
     )
 }
+
+const SearchBar = () => {
+    return (
+        <div>   
+            <TextField variant="outlined" className="inputfield"/>
+            <Button color="primary" variant="contained">Search</Button>
+         </div>
+    )
+}
+
+const MusicBox = ({music}) => {
+    return (
+        <div className="music_box">
+            {music.map((base, index) => {
+                return (
+                    <div>
+                        <Box className="box" sx={{ width: 200, height: 200 , '&:hover': { backgroundColor: 'primary.main', opacity: [0.9, 0.8, 0.7]},}}>
+                            <h1>{base.name}</h1>
+                            <p>artist : {base.artist}</p>
+                            <p>genre : {base.genre}</p>
+                            <p>file : {base.file}</p>
+                        </Box>
+                    </div>
+                )
+            })}
+            
+        </div>
+    )
+}
+
 
 
 
