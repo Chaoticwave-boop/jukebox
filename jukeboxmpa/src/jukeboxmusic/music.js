4947bb5ec69249efcce244880ba42ca9fc03a51b
Chaoticwave-boop <bren.stoute@hotmail.com>
2023-06-23 14:21:32 +0200

    post added plus delete added for library

--- jukeboxmpa/src/jukeboxmusic/music.js
+++ jukeboxmpa/src/jukeboxmusic/music.js
@@ -3,15 +3,28 @@ import './music.scss';
 import { useState, useEffect } from "react";
 import AddIcon from '@mui/icons-material/Add';
 import {TextField, Button, Box} from '@mui/material';
+import MuiAlert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
 
 
 const Music = () => {
     const [ music, setMusic ] = useState();
+    const [ library, setLibrary ] = useState();
 
-    useEffect(() => {
-        fetch("/api").then (
+      useEffect(() => {
+        fetch("/api/music").then (
           response => response.json()
+          
         ).then((data) => setMusic(data));
+
+      }, []);
+
+      useEffect(() => {
+        fetch("/api/library/content").then (
+          response => response.json()
+          
+        ).then((data) => setLibrary(data));
+  
       }, []);
 
     return (
@@ -19,15 +32,14 @@ const Music = () => {
             <div className="musictext">
                 <h1>Add your music and search</h1>
             
-                <p>{!music ? "Loading..." : <MusicBox music={music} />}</p>
+                <p>{!music ? "Loading..." : <MusicBox music={music} library={library}/>}</p>
             </div>
 
         </div>
     )
 }
 
-
-const MusicBox = ({ music }) => {
+const MusicBox = ({ music, library }) => {
     const [ Searched, SetSearched ] = useState();
     const [ filteredmusic, setFilteredMusic ] = useState(music);
         
@@ -35,7 +47,7 @@ const MusicBox = ({ music }) => {
        
         if (Searched) {
             const newGameList = [...music].filter((music) => {
-                if (music.name.toLowerCase().includes(Searched.toLowerCase())){
+                if (music.Name.toLowerCase().includes(Searched.toLowerCase())){
                     return true
                 } else {
                     return false
@@ -55,25 +67,47 @@ const MusicBox = ({ music }) => {
                 <Button color="primary" variant="contained" onClick={() => FilteredMusic()}>Search</Button>
             </div>
 
-            <p>{!music ? "Loading..." : <ShowMusic music={filteredmusic} Searched={Searched}/>}</p>
+            <p>{!music ? "Loading..." : <ShowMusic music={filteredmusic} Searched={Searched} library={library}/>}</p>
         </div>
     )
 }
 
 
 
-const ShowMusic = ({music, Searched}) => {
+const ShowMusic = ({ music, library }) => {
+    const [ open , setOpen ] = useState(false);
+
+    const AddToLibrary = (base) => {
+        fetch('/api/library', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                Name: base.Name ,
+                Artist: base.Artist,
+                Genre: base.Genre 
+            })
+        }).then(() => setOpen(true))
+    }
+
+
     
    return (
     <div className="music_box">
+        <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
+            <MuiAlert onClose={() => setOpen(false)} severity="success" sx={{ width: '100%' }}  variant="filled">
+                Added
+            </MuiAlert>
+        </Snackbar>
         {music.map((base, index) => {
             return (
                 <div >
                     <Box className="box" sx={{ width: 200, height: 200 , '&:hover': { backgroundColor: 'primary.main', opacity: [0.9, 0.8, 0.7]},}}>
-                        <h1>{base.name}</h1>
-                        <p>artist : {base.artist}</p>
-                        <p>genre : {base.genre}</p>
-                        <Button ><AddIcon/></Button>
+                        <h1>{base.Name}</h1>
+                        <p>artist : {base.Artist}</p>
+                        <p>genre : {base.Genre}</p>
+                        <Button onClick={() => AddToLibrary(base)}><AddIcon/></Button>
                     </Box>
                 </div>
             )
--- jukeboxmpa/src/library/libary.js
+++ jukeboxmpa/src/library/libary.js
@@ -1,16 +1,110 @@
 import React from "react";
-
-
+import './library.scss';
+import { useState, useEffect } from "react";
+import {TextField, Button, Box} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import MuiAlert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
 
 
 const Library = () => {
+    const [ library, setLibrary ] = useState();
+
+    useEffect(() => {
+      fetch("/api/library/content").then (
+        response => response.json()
+        
+      ).then((data) => setLibrary(data));
+
+    }, []);
+
+    return (
+        <div className="library_page">
+            <div className="musictext">
+                <h1>Your music</h1>
+                <p>{!library ? "Loading..." : <LibraryBox library={library} />}</p>
+            </div>
+        </div>
+    )
+}
+ 
+const LibraryBox = ({library}) => {
+
+    const [ Searched, SetSearched ] = useState();
+    const [ filteredmusic, setFilteredMusic ] = useState(library);
+        
+    const FilteredMusic = () => {
+    
+        if (Searched) {
+            const newGameList = [...library].filter((library) => {
+                if (library.Name.toLowerCase().includes(Searched.toLowerCase())){
+                    return true
+                } else {
+                    return false
+                }
+            });
+
+            setFilteredMusic(newGameList);
+        } else {
+            setFilteredMusic(library)
+        }
+    }
+
     return (
         <div>
-            <h1>your added music</h1>
+            <div>   
+                <TextField variant="outlined" className="inputfield" onChange={(event) => SetSearched(event.target.value)}/>
+                <Button color="primary" variant="contained" onClick={() => FilteredMusic()}>Search</Button>
+            </div>
+
+            <p>{!library ? "Loading..." : <ShowMusic music={filteredmusic} Searched={Searched}/>}</p>
         </div>
     )
 }
 
 
+
+
+const ShowMusic = ({ music }) => {
+    const [ currentMusic, setCurrentMusic ] = useState(music);
+    const [ open , setOpen ] = useState(false)
+
+    const getNewMusic = () => {
+        fetch("/api/library/content").then (
+            response => response.json()
+            
+        ).then((data) => setCurrentMusic(data));
+    }
+
+    const DeleteMusic = (base) => {
+        console.log(base)
+        fetch(`/api/library/delete/${base.id}`, {
+            method: 'DELETE',
+        }).then((response) => response.json()).then(() => getNewMusic()).then(() => setOpen(true))
+    }
+    
+    return (
+     <div className="music_box">
+        <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
+            <MuiAlert onClose={() => setOpen(false)} severity="error" sx={{ width: '100%' }}  variant="filled">
+                Deleted
+            </MuiAlert>
+        </Snackbar>
+         {currentMusic.map((base, index) => {
+             return (
+                 <div >
+                    <Box className="box" sx={{ width: 200, height: 200 , '&:hover': { backgroundColor: 'primary.main', opacity: [0.9, 0.8, 0.7]},}}>
+                        <h1>{base.Name}</h1>
+                        <p>artist : {base.Artist}</p>
+                        <p>genre : {base.Genre}</p>
+                        <Button onClick={() => DeleteMusic(base)}><DeleteIcon/></Button>
+                    </Box>
+                 </div>
+             )
+         })}
+     </div>
+    )
+ }
+ 
 
 export default Library
